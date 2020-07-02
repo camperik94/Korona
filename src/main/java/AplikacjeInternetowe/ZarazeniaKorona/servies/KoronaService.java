1c626dfa40e4f8a58ca24f0b5036c05a9ed7382b
camperik94 <tomaszjach94@gmail.com>
2020-07-02 08:54:30 +0200

    dodanie metody pobieranieDanych

--- src/main/java/AplikacjeInternetowe/ZarazeniaKorona/servies/KoronaService.java
+++ src/main/java/AplikacjeInternetowe/ZarazeniaKorona/servies/KoronaService.java
@@ -1,4 +1,25 @@
 package AplikacjeInternetowe.ZarazeniaKorona.servies;
 
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
 public class KoronaService {
+    private static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    @PostConstruct
+    public void pobranieDanych() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient(); // sluzy do wysylania i otrzymywania zapytan/odpowiedzi
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(DATA_URL))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
+    }
 }
