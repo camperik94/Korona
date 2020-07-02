cc199fc59ca02567ba587fb0dc27cfcb9c603cd9
camperik94 <tomaszjach94@gmail.com>
2020-07-02 08:32:35 +0200

    start projektu

---
+++ src/main/java/AplikacjeInternetowe/ZarazeniaKorona/ZarazeniaKoronaApplication.java
@@ -0,0 +1,13 @@
+package AplikacjeInternetowe.ZarazeniaKorona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarazeniaKoronaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarazeniaKoronaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AplikacjeInternetowe/ZarazeniaKorona/ZarazeniaKoronaApplicationTests.java
@@ -0,0 +1,13 @@
+package AplikacjeInternetowe.ZarazeniaKorona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarazeniaKoronaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
